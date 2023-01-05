42e0ec1036751cf4fb71821806f44c26c9fc4446
ribeiroAllison <allisonribeiro@gmail.com>
2023-01-05 12:27:24 -0300

    working tests

--- luhnChecker.js
+++ luhnChecker.js
@@ -19,11 +19,8 @@ const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
 const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
-// An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
-
-// Creating a validity checker based on Luhn algorithm:
+// Creating a validity checker object based on Luhn algorithm:
 const LuhnChecker = {
     _cardNumber: [],
 
@@ -96,7 +93,7 @@ const LuhnChecker = {
 
 LuhnChecker.cardNumber = valid1;
 
-console.log(LuhnChecker.check());
+
 
 module.exports = LuhnChecker;
 
--- test/luhnChecker_test.js
+++ test/luhnChecker_test.js
@@ -3,6 +3,22 @@ const LuhnChecker = require('../luhnChecker.js');
 const assert = require('assert');
 
 describe('LuhnChecker', () =>{
+    describe('.checkDigit', () =>{
+        it('extracts the last number of the input array', () =>{
+            //setup
+            const inputArray = [1,2,3,4,5];
+            const expectedResult = 5;
+
+            //exercise
+            LuhnChecker.cardNumber = inputArray;
+            const result = LuhnChecker.ckeckDigit();
+
+            //verify
+            assert.strictEqual(result, expectedResult);
+            
+        })
+    })
+
     describe('.newValid', () =>{
         it('receives an array, if the array length is even it multiplies the elements with even index by two', ()=>{
             //setup
@@ -19,8 +35,8 @@ describe('LuhnChecker', () =>{
 
         it('receives an array, if the array length is odd it multiplies the elements with odd index by two', ()=>{
             //setup
-            const inputArray = [1,2,3,4,5];
-            const expectedResults = [1,4,3,8,5];
+            const inputArray = [0, 5, 2, 6, 4];
+            const expectedResults = [0, 10, 2, 12, 4];
             LuhnChecker.cardNumber = inputArray;
     
             //exercise
@@ -33,13 +49,12 @@ describe('LuhnChecker', () =>{
     });
 
     describe('.adjValid', () =>{
-        it('checks if there are any numbers greater than 9 at the a new array resulted from calling .newvalid(), if there are subtract 9 from them', () =>{
-
-            //setup
-            const inputArray = [6,2,5,1];
+        it('checks if there are any numbers greater than 9 at the a new array resulted from calling .newvalid(), if there are subtract 9 from them. Removes last number from the resulting array', () =>{
+            
+                //setup
+            const inputArray = [0, 5, 2, 6, 4];
             LuhnChecker.cardNumber = inputArray;
-            LuhnChecker.newValid();
-            const expectedResult = [3,2,1,1]
+            const expectedResult = [0, 1, 2, 3]
 
             //exercise
             const result = LuhnChecker.adjValid();
@@ -47,8 +62,60 @@ describe('LuhnChecker', () =>{
             //verify
             assert.deepStrictEqual(result, expectedResult);
 
+            
+
         })
     });
+
+    describe('.sumUp', () =>{
+        it('sums all the numbers of the resulting array from .adjValid() calling and adds the result of .checkDigit() to this total', () =>{
+
+            //setup
+            const inputArray = [0, 5, 2, 6, 4];
+            LuhnChecker.cardNumber = inputArray;
+            const expectedResult = 10;
+
+            //exercise
+            const result = LuhnChecker.sumUp();
+
+            //verify
+            assert.strictEqual(result, expectedResult);
+        })
+
+        
+    })
+
+    describe('.check', () =>{
+        it('checks if the resulting number from .sumUp() % 10 iquals 0, if so return TRUE', () =>{
+
+            //setup
+            const inputArray = [0, 5, 2, 6, 4];
+            LuhnChecker.cardNumber = inputArray;
+            const expectedResult = true;
+
+            //exercise
+            const result = LuhnChecker.check();
+
+            //verify
+            assert.strictEqual(result, expectedResult);
+
+        });
+
+        it('checks if the resulting number from .sumUp() % 10 iquals 0, if NOT return FALSE', () =>{
+
+            //setup
+            const inputArray = [0, 5, 2, 6, 5];
+            LuhnChecker.cardNumber = inputArray;
+            const expectedResult = false;
+
+            //exercise
+            const result = LuhnChecker.check();
+
+            //verify
+            assert.strictEqual(result, expectedResult);
+
+        });
+    })
 
 
     
